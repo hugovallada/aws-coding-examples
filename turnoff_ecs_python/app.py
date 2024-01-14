35475455091990244c1fe2fad22b2356efdf08c9
Hugo <valladahugo@gmail.com>
2024-01-14 08:21:41 -0300

    Remoção de espaçamento vertical

--- turnoff_ecs_python/app.py
+++ turnoff_ecs_python/app.py
@@ -5,14 +5,11 @@ from logging import info
 def lambda_handler(event, context):
     ecs_client = boto3.client('ecs')
     service_arn = 'service_arn'
-
     response = ecs_client.update_service(
         service=service_arn,
         desiredCount=0
     )
-
     info(f'O serviço {service_arn} foi desligado!')
-
     return {
         'statusCode': 200,
         'body': response
